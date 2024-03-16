e250e80d0a481f3478a92c27fdb9241776ae10a1
sahib2410 <contactsahib24@gmail.com>
2024-03-16 12:32:58 +0530

    madechangesonjoy

--- src/components/section.js
+++ src/components/section.js
@@ -10,10 +10,10 @@ const Section = () => {
         <div className='window'>hello</div>
     </section>
     <section>
-        <div className='window'>hello</div>
-        <div className='window'>hello</div>
-        <div className='window'>hello</div>
-        <div className='window'>hello</div>
+        <div className='window'>helloguys</div>
+        <div className='window'>helloguys</div>
+        <div className='window'>helloguys</div>
+        <div className='window'>helloguys</div>
     </section>
     </>
   )
